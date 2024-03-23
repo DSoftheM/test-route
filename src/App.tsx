6022ae233be6ddd867a38ebe5073dbd6772345f1
ArturGainutdinov <gajnutdinov.a.l@ft-soft.ru>
2024-03-23 17:55:54 +0500

    f

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import "./App.css";
+import { Link, Outlet, Route, Routes } from "react-router-dom";
+
+function App() {
+    return (
+        <Routes>
+            <Route element={<Header />}>
+                <Route path="/" element={<p>/</p>} />
+                <Route path="/start" element={<p>Start</p>} />
+                <Route path="/index" element={<p>index</p>} />
+            </Route>
+        </Routes>
+    );
+}
+
+function Header() {
+    return (
+        <div>
+            <Link to={"/"}>/</Link>
+            <hr />
+            <Link to={"/start"}>/start</Link>
+            <hr />
+            <Link to={"/index"}>/index</Link>
+            <hr />
+            <Outlet />
+        </div>
+    );
+}
+
+export default App;
+
